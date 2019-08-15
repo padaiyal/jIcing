fc855c0260330032625071d4ce4082e69307fedf
Ranjan Mohan <ranjanmj@gmail.com>
2019-08-14 23:30:11 -0500

    Added I18NUtility
    Closes #17
    This utility is used to retrieve messages relevant to a specific locale.
    The requirements are as follows:
    
        Retrieve the message corresponding to the key.
        Add documentation comments.
        Internationalize messages.
        Convert all print statements to log statements.
        Add test coverage for all requirements against Windows and Linux (Ubuntu).

---
+++ src/main/java/utilities/I18NUtility.java
@@ -0,0 +1,89 @@
+package utilities;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class I18NUtility {
+
+    private static String language = "en";// TODO: Add to SettingUtility
+    private static String region = "US";// TODO: Add to SettingUtility
+    private static Locale locale = new Locale(language, region);
+    private static Set<String> resourceBundleNames = Collections.synchronizedSet(new HashSet<>());
+    private static Set<ResourceBundle> resourceBundles = Collections.synchronizedSet(new HashSet<>());
+
+    static {
+        resourceBundleNames.add("I18N_Resource_Bundle");
+        buildResourceBundlesSet();
+    }
+
+    /**
+     * Build the ResourceBundle objects from the currently specified resource bundle names in the resourceBundleNames set
+     */
+    private static void buildResourceBundlesSet() {
+        resourceBundles.clear();
+        resourceBundles.addAll(
+                resourceBundleNames.stream()
+                        .map(resourceBundleName -> ResourceBundle.getBundle(resourceBundleName, locale))
+                        .collect(Collectors.toSet())
+        );
+    }
+
+    /**
+     * Retrieves the value corresponding to the key specified.
+     * If multiple resource bundles contain the same key, the first resource
+     * bundle (Decided by the order in which the resource bundle is added) in
+     * which the key is present is used.
+     * @param key The key to retrieve the value for
+     * @return The value corresponding to the key specified
+     */
+    public static String getString(String key) {
+        Optional<String> value = resourceBundles.stream()
+            .map(resourceBundle -> {
+                String tempValue;
+                try {
+                    tempValue = resourceBundle.getString(key);
+                }
+                catch (MissingResourceException e) {
+                    tempValue = null;
+                }
+                return tempValue;
+            })
+            .filter(Objects::nonNull)
+            .findFirst();
+        if(value.isPresent()) {
+            return value.get();
+        }
+        else {
+            throw new MissingResourceException(
+                String.format("Can't find resource for bundle %s", key),
+                I18NUtility.class.getName(),
+                key
+            );
+        }
+    }
+
+    /**
+     * Add a specified resource bundle
+     * @param resourceBundleName Resource bundle to add
+     */
+    public synchronized static void addResourceBundle(String resourceBundleName) {
+        resourceBundleNames.add(resourceBundleName);
+        buildResourceBundlesSet();
+    }
+
+    /**
+     * Remove a specified resource bundle
+     * @param resourceBundleName Resource bundle to remove
+     */
+    public synchronized static void removeResourceBundle(String resourceBundleName) {
+        boolean resourceBundlePresent = resourceBundleNames.remove(resourceBundleName);
+        if(!resourceBundlePresent) {
+            throw new MissingResourceException(
+                    String.format("Can't find resource bundle %s", resourceBundleName),
+                    I18NUtility.class.getName(),
+                    resourceBundleName
+            );
+        }
+        buildResourceBundlesSet();
+    }
+}
