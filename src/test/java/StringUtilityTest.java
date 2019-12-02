4ed5fa245a8b794bd2e3bcec73e386e3c53b245c
Ranjan Mohan <ranjanmj@gmail.com>
2019-12-01 19:25:04 -0500

    Added StringUtility
    Closes #56.
    The StringUtility has the following functionality:
        -Repeat an input string a specified number of times.

---
+++ src/test/java/StringUtilityTest.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import utilities.StringUtility;
+
+
+class StringUtilityTest {
+
+    /**
+     * Test repeatString functionality with positive, negative, 0 repeat count and null string value.
+     * @param string String to repeat.
+     * @param repeatCount Number of times too repeat.
+     * @param expectedOutput Expected output.
+     */
+    @ParameterizedTest
+    @CsvSource(
+        {
+            "lol, 5, lollollollollol",
+            "lol, 0, ''",
+            "lol, -1, ''",
+            "'', 10000, ''"
+        }
+    )
+    void testRepeatString(String string, long repeatCount, String expectedOutput) {
+        try {
+            String actualOutput = StringUtility.repeat(string, repeatCount);
+            Assertions.assertEquals(expectedOutput, actualOutput);
+        }
+        catch(IllegalArgumentException e) {
+            Assertions.assertTrue(repeatCount < 0);
+        }
+    }
+}
+
