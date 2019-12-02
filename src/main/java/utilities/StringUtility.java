42292c68334fff8e37c4a044380ba0f3c3d30e7e
Ranjan Mohan <ranjanmj@gmail.com>
2019-12-01 19:25:04 -0500

    Added StringUtility
    Closes #56.
    The StringUtility has the following functionality:
        -Repeat an input string a specified number of times.

---
+++ src/main/java/utilities/StringUtility.java
@@ -0,0 +1,27 @@
+package utilities;
+
+import java.util.stream.LongStream;
+
+public class StringUtility {
+
+    /**
+     * Repeats the given string the specified number of times and returns it as a single string output.
+     * @param str String too repeat
+     * @param repeatCount Number of times to repeat str
+     * @return The string with repeatCount contiguous occurrences of str.
+     */
+    public static String repeat(String str, long repeatCount) {
+        if(repeatCount < 0) {
+            throw new IllegalArgumentException(
+                    I18NUtility.getFormattedString(
+                            "utilities.StringUtility.repeatString.error.negativeRepeatCount",
+                            repeatCount
+                    )
+            );
+        }
+        return LongStream.range(0, repeatCount)
+                .mapToObj(i ->  str)
+                .reduce("", (str1, str2) -> str1 + str2);
+    }
+
+}
