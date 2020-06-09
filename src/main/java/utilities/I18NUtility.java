c81d9d1238a8496aedd8105d380691da6ed8bb6a
Ranjan Mohan <ranjanmj@gmail.com>
2020-06-09 19:18:10 -0400

    Add provision to pass class to retrieve classpath from for I18N resource bundle
    Closes #84.

--- src/main/java/utilities/I18NUtility.java
+++ src/main/java/utilities/I18NUtility.java
@@ -81,9 +81,18 @@ public class I18NUtility {
      * @param resourceBundleName Resource bundle to add
      */
     public synchronized static void addResourceBundle(String resourceBundleName) {
+        addResourceBundle(I18NUtility.class, resourceBundleName);
+    }
+
+    /**
+     * Add a specified resource bundle
+     * @param cls Class to retrieve the classpath from
+     * @param resourceBundleName Resource bundle to add
+     */
+    public synchronized static void addResourceBundle(Class cls, String resourceBundleName) {
         // Cannot internationalize as the required I18N string cannot be found without adding the relevant resource bundle
-        logger.info("Adding resource bundle - " + resourceBundleName);
-        ResourceBundle resourceBundle = ResourceBundle.getBundle(I18NUtility.class.getPackageName() + "." + resourceBundleName, locale);
+        logger.info("Adding resource bundle - " + resourceBundleName + " using the classpath " + cls.getPackageName());
+        ResourceBundle resourceBundle = ResourceBundle.getBundle(cls.getPackageName() + "." + resourceBundleName, locale);
         resourceBundles.put(resourceBundleName, resourceBundle);
     }
 
