04350f535d7234c0d30a69aecd29c060ba8b876a
Ranjan Mohan <ranjanmj@gmail.com>
2020-11-16 12:47:28 -0500

    Added Mac OS X support for ShellUtility and ShellUtilityTest
    Closes #95.
    Added ZSH support to ShellUtility so that it can be supported in
    MAC OS X.

--- src/main/java/utilities/OS.java
+++ src/main/java/utilities/OS.java
@@ -1,21 +1,38 @@
 package utilities;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public enum OS {
     WINDOWS,
     LINUX,
+    MAC_OS_X,
     UNKNOWN;
 
+    private static Logger logger = LogManager.getLogger(OS.class);
+
+    /**
+     * Detects the OS of the machine in which this method is called.
+     * @return The detected OS. If it's unable to detect, it will return UNKNOWN.
+     */
     public static OS getOs() {
-        String os = System.getProperty("os.name").toUpperCase();
-        System.out.println(os.contains("LINUX"));
-        if(os.contains("LINUX")) {
-            return LINUX;
+        String osString = System.getProperty("os.name")
+                .toUpperCase();
+        logger.debug("OS string: " + osString);
+        OS detectedOS;
+        if(osString.contains("LINUX")) {
+            detectedOS = LINUX;
         }
-        else if(os.contains("WINDOWS")) {
-            return WINDOWS;
+        else if(osString.contains("WINDOWS")) {
+            detectedOS = WINDOWS;
+        }
+        else if(osString.contains("MAC OS X")) {
+            detectedOS = MAC_OS_X;
         }
         else {
-            return UNKNOWN;
+            detectedOS = UNKNOWN;
         }
+        logger.info("OS detected: " + detectedOS);
+        return detectedOS;
     }
 }
